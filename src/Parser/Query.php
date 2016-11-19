121a126c1a555fbaeb4debe5659398dc8d8e8017
jacobemerick <jpemeric@gmail.com>
2016-11-19 06:38:21 -0700

    Pulls out explode logic into trait

--- src/Parser/Query.php
+++ src/Parser/Query.php
@@ -2,11 +2,12 @@
 
 namespace AvalancheDevelopment\SwaggerRouterMiddleware\Parser;
 
-use Exception;
 use Psr\Http\Message\RequestInterface as Request;
 
 class Query implements ParserInterface
 {
+
+    use ExplodeTrait;
 
     /** @var Request */
     protected $request;
@@ -75,48 +76,5 @@ class Query implements ParserInterface
         }
 
         return $params;
-    }
-
-    /**
-     * @param mixed $value
-     * @param array $parameter
-     * @return array
-     */
-    protected function explodeValue($value, array $parameter)
-    {
-        $delimiter = $this->getDelimiter($parameter);
-        return preg_split("@{$delimiter}@", $value);
-    }
-
-    /**
-     * @param array $parameter
-     * @return string
-     */
-    protected function getDelimiter(array $parameter)
-    {
-        $collectionFormat = 'csv';
-        if (isset($parameter['collectionFormat'])) {
-            $collectionFormat = $parameter['collectionFormat'];
-        }
-
-        switch ($collectionFormat) {
-            case 'csv':
-                $delimiter = ',';
-                break;
-            case 'ssv':
-                $delimiter = '\s';
-                break;
-            case 'tsv':
-                $delimiter = '\t';
-                break;
-            case 'pipes':
-                $delimiter = '|';
-                break;
-            default:
-                throw new Exception('Invalid collection format value defined in swagger');
-                break;
-        }
-
-        return $delimiter;
     }
 }
