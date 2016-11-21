60a1353920c35523429dcdee2d11af2a792f60ff
jacobemerick <jpemeric@gmail.com>
2016-11-21 05:41:03 -0700

    Adds catch for uploaded files, plus test

--- tests/unit/src/Parser/FormTest.php
+++ tests/unit/src/Parser/FormTest.php
@@ -4,6 +4,7 @@ namespace AvalancheDevelopment\SwaggerRouterMiddleware\Parser;
 
 use PHPUnit_Framework_TestCase;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
 use ReflectionClass;
 
 class FormTest extends PHPUnit_Framework_TestCase
@@ -39,15 +40,88 @@ class FormTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeSame($mockParameter, 'parameter', $formParser);
     }
 
+    public function testGetValueReturnsNullIfUnmatchedFile()
+    {
+        $mockRequest = $this->createMock(ServerRequestInterface::class);
+        $mockRequest->expects($this->once())
+            ->method('getUploadedFiles')
+            ->willReturn([]);
+        $mockRequest->expects($this->never())
+            ->method('getParsedBody');
+
+        $mockParameter = [
+            'name' => 'some_variable',
+            'type' => 'file',
+        ];
+
+        $reflectedFormParser = new ReflectionClass(Form::class);
+        $reflectedRequest = $reflectedFormParser->getProperty('request');
+        $reflectedRequest->setAccessible(true);
+        $reflectedParameter = $reflectedFormParser->getProperty('parameter');
+        $reflectedParameter->setAccessible(true);
+
+        $formParser = $this->getMockBuilder(Form::class)
+            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock();
+
+        $reflectedRequest->setValue($formParser, $mockRequest);
+        $reflectedParameter->setValue($formParser, $mockParameter);
+
+        $result = $formParser->getValue();
+
+        $this->assertNull($result);
+    }
+
+    public function testGetValueReturnsFileIfMatched()
+    {
+        $expectedValue = $this->createMock(UploadedFileInterface::class);
+
+        $mockRequest = $this->createMock(ServerRequestInterface::class);
+        $mockRequest->expects($this->once())
+            ->method('getUploadedFiles')
+            ->willReturn([
+                'some_variable' => $expectedValue,
+            ]);
+        $mockRequest->expects($this->never())
+            ->method('getParsedBody');
+
+        $mockParameter = [
+            'name' => 'some_variable',
+            'type' => 'file',
+        ];
+
+        $reflectedFormParser = new ReflectionClass(Form::class);
+        $reflectedRequest = $reflectedFormParser->getProperty('request');
+        $reflectedRequest->setAccessible(true);
+        $reflectedParameter = $reflectedFormParser->getProperty('parameter');
+        $reflectedParameter->setAccessible(true);
+
+        $formParser = $this->getMockBuilder(Form::class)
+            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock();
+
+        $reflectedRequest->setValue($formParser, $mockRequest);
+        $reflectedParameter->setValue($formParser, $mockParameter);
+
+        $result = $formParser->getValue();
+
+        $this->assertSame($expectedValue, $result);
+   }
+
     public function testGetValueReturnsNullIfUnmatched()
     {
         $mockRequest = $this->createMock(ServerRequestInterface::class);
+        $mockRequest->expects($this->never())
+            ->method('getUploadedFiles');
         $mockRequest->expects($this->once())
             ->method('getParsedBody')
             ->willReturn([]);
 
         $mockParameter = [
             'name' => 'some_variable',
+            'type' => 'string',
         ];
 
         $reflectedFormParser = new ReflectionClass(Form::class);
