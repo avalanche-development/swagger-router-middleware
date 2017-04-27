727240269b02eeedd7d604d4f687a578fb47cd52
jacobemerick <jpemeric@gmail.com>
2017-04-26 20:10:24 -0700

    Adds tests around new json handling

--- tests/unit/src/Parser/BodyTest.php
+++ tests/unit/src/Parser/BodyTest.php
@@ -27,14 +27,13 @@ class BodyTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeSame($mockRequest, 'request', $bodyParser);
     }
 
-    public function testGetValueReturnsStringIfContentUnspecified()
+    public function testGetValueReturnsNullIfNoBody()
     {
         $mockRequest = $this->createMock(RequestInterface::class);
         $mockRequest->method('getBody')
-            ->willReturn(123);
-        $mockRequest->method('getHeader')
-            ->with('Content-Type')
-            ->willReturn([]);
+            ->willReturn('');
+        $mockRequest->expects($this->never())
+            ->method('getHeader');
 
         $reflectedBodyParser = new ReflectionClass(Body::class);
         $reflectedRequest = $reflectedBodyParser->getProperty('request');
@@ -56,6 +55,184 @@ class BodyTest extends PHPUnit_Framework_TestCase
 
         $result = $bodyParser->getValue();
 
+        $this->assertNull($result);
+    }
+
+    public function testGetValueChecksForJsonHeader()
+    {
+        $mockHeader = [
+            'some header',
+        ];
+
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getBody')
+            ->willReturn('some string');
+        $mockRequest->method('getHeader')
+            ->with('Content-Type')
+            ->willReturn($mockHeader);
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedRequest = $reflectedBodyParser->getProperty('request');
+        $reflectedRequest->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'checkJsonHeader',
+                'parseJson',
+            ])
+            ->getMock();
+        $bodyParser->expects($this->once())
+            ->method('checkJsonHeader')
+            ->with($mockHeader[0]);
+
+        $reflectedRequest->setValue($bodyParser, $mockRequest);
+
+        $bodyParser->getValue();
+    }
+
+    public function testGetValueParsesJsonIfJsonHeaderFound()
+    {
+        $mockBody = 'some body';
+        $parsedBody = 'some parsed body';
+
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getBody')
+            ->willReturn($mockBody);
+        $mockRequest->method('getHeader')
+            ->with('Content-Type')
+            ->willReturn([
+                'some header',
+            ]);
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedRequest = $reflectedBodyParser->getProperty('request');
+        $reflectedRequest->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'checkJsonHeader',
+                'parseJson',
+            ])
+            ->getMock();
+        $bodyParser->method('checkJsonHeader')
+            ->willReturn(true);
+        $bodyParser->expects($this->once())
+            ->method('parseJson')
+            ->with($mockBody)
+            ->willReturn($parsedBody);
+
+        $reflectedRequest->setValue($bodyParser, $mockRequest);
+
+        $result = $bodyParser->getValue();
+
+        $this->assertSame($parsedBody, $result);
+    }
+
+    public function testGetValueReturnsStringIfNoJsonHeaderFound()
+    {
+        $mockRequest = $this->createMock(RequestInterface::class);
+        $mockRequest->method('getBody')
+            ->willReturn(123);
+        $mockRequest->method('getHeader')
+            ->with('Content-Type')
+            ->willReturn([
+                'some header',
+            ]);
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedRequest = $reflectedBodyParser->getProperty('request');
+        $reflectedRequest->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'checkJsonHeader',
+                'parseJson',
+            ])
+            ->getMock();
+        $bodyParser->method('checkJsonHeader')
+            ->willReturn(false);
+        $bodyParser->expects($this->never())
+            ->method('parseJson');
+
+        $reflectedRequest->setValue($bodyParser, $mockRequest);
+
+        $result = $bodyParser->getValue();
+
         $this->assertSame('123', $result);
+    }
+
+    public function testCheckJsonHeaderReturnsTrueIfJsonHeaderFound()
+    {
+        $contentType = 'application/json';
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedCheckJsonHeader = $reflectedBodyParser->getMethod('checkJsonHeader');
+        $reflectedCheckJsonHeader->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedCheckJsonHeader->invokeArgs($bodyParser, [ $contentType ]);
+
+        $this->assertTrue($result);
+    }
+
+    public function testCheckJsonHeaderReturnsFalseIfNoJsonHeadersFound()
+    {
+        $contentType = 'application/xml';
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedCheckJsonHeader = $reflectedBodyParser->getMethod('checkJsonHeader');
+        $reflectedCheckJsonHeader->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedCheckJsonHeader->invokeArgs($bodyParser, [ $contentType ]);
+
+        $this->assertFalse($result);
+    }
+
+    public function testParseJsonReturnsNullIfInvalidJson()
+    {
+        $body = 'some string';
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedParseJson = $reflectedBodyParser->getMethod('parseJson');
+        $reflectedParseJson->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedParseJson->invokeArgs($bodyParser, [ $body ]);
+
+        $this->assertNull($result);
+    }
+
+    public function testParseJsonReturnsJsonIfValidJson()
+    {
+        $body = '{"key":"some value"}';
+
+        $parsedBody = [
+            'key' => 'some value',
+        ];
+
+        $reflectedBodyParser = new ReflectionClass(Body::class);
+        $reflectedParseJson = $reflectedBodyParser->getMethod('parseJson');
+        $reflectedParseJson->setAccessible(true);
+
+        $bodyParser = $this->getMockBuilder(Body::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result = $reflectedParseJson->invokeArgs($bodyParser, [ $body ]);
+
+        $this->assertSame($parsedBody, $result);
     }
 }
